61dbb2b9923629606a6d25f80b6d5063c6cdee28
kurosekoma <1169081504@qq.com>
2021-03-11 18:35:53 +0800

    Create Task.php

---
+++ session2/Task.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+    <title>Bootstrap</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css"><!--- font-awesome CDN of cn --->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!--- font-awesome --->
+
+</head>
+<body>
+    <?php
+    echo"<>";
+
+
+
+    ?>
+
+</body>
+
+</html>
