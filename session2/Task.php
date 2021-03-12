7ff29b722c32875233c3ceb8b5045d296bcec87f
kurosekoma <1169081504@qq.com>
2021-03-12 16:18:43 +0800

    add base files
    add base files

--- session2/Task.php
+++ session2/Task.php
@@ -1,20 +1,34 @@
 <!DOCTYPE html>
 <head>
-    <title>Bootstrap</title>
+    <title>Bootstrap Exercise 3</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css"><!--- font-awesome CDN of cn --->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!--- font-awesome --->
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
+    <div class="container center">
+    <h2>Exercise 3</h2>
+    <div class="row">
+    <div class='col-12'><h3>Part 1</h3></div>
+    </div>
+    <div class="row">
+    <div class='col-md-2 col-sm-6 col-xs-12'><h3>Grooming</h3><p>Our</p></div>
+    <div class='col-lg-2 col-md-4 col-sm-6 col-xs-12'><h3>GeneralHealth</h3><p>We offer special</p></div>
+    
+    <div class='col-lg-2 col-md-4 col-sm-6 col-xs-12'><h3>Nutrition</h3><p>Let out nutrition experts review your pet's det and prescrite a custom nutrition plan for optimum health and disesse prevention.</p></div>
+    <div class='col-lg-2 col-md-4 col-sm-6 col-xs-12'><h3>Pest Control</h3><p>We offer the latest advances in safe and effcetve prevention and treatment of sicks, wigrra, heat worm, and other parastes.</p></div>
+    <div class='col-lg-2 col-md-4 col-sm-6 col-xs-12'><h3>Vaccinations</h3><p>Our vaccinations are experiences in modern vaccination protocals that prevert many of the deadest diseasses in pets.</p></div>
+    </div>
+    
     <?php
     echo"<>";
-
-
-
     ?>
 
 </body>
