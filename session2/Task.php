bcbf8940c43358ca6a4e27f2b0e552f7e09dbbf4
kurosekoma <1169081504@qq.com>
2021-03-12 16:55:37 +0800

    Add css(height)

--- session2/Task.php
+++ session2/Task.php
@@ -11,22 +11,34 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <style>
+        .bannerHeighth{
+            height: 300px;
+        }
+        .menuItems{
+            height: 80px;
+        }
+        .siteContent{
+            height: 300px;
+        }
+    </style>
 </head>
 <body>
     <div class="container center">
     <h2>Exercise 3</h2>
     <div class="row">
-        <section class='col-12' height='300'><p>Site Banner (Logo etc.)</p></section>
+        <section class='col-12 bannerHeighth'><p>Site Banner (Logo etc.)</p></section>
     </div>
     <div class="row">
         <section class='col-md-3 col-sm-12'>
         <div class="row">
-            <div class='col-md-12 col-sm-4' height='80'>Menu Item 1</div>
-            <div class='col-md-12 col-sm-4' height='80'>Menu Item 2</div>
-            <div class='col-md-12 col-sm-4' height='80'>Menu Item 3</div>
+            <div class='col-md-12 col-sm-4 menuItems'>Menu Item 1</div>
+            <div class='col-md-12 col-sm-4 menuItems'>Menu Item 2</div>
+            <div class='col-md-12 col-sm-4 menuItems'>Menu Item 3</div>
         </div>
         </section>
-        <section class='col-md-9 col-12' height='300'>Random Site Content</section>
+        <section class='col-md-9 col-12 siteContent'>Random Site Content</section>
     </div>
 
 </body>
