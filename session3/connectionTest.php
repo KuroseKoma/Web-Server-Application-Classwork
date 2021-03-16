f269a933bf4462e5d3c05332c4a5214e1fe5f8d0
kurosekoma <1169081504@qq.com>
2021-03-16 10:35:54 +0800

    add SQL and print the customer names

--- session3/connectionTest.php
+++ session3/connectionTest.php
@@ -10,11 +10,19 @@
   $servername = "localhost";
   $dbusername = "root";
   $dbpassword = "";
-  $dbname = "s17160495";
+  $dbname = "classicmodels";
   $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
   mysqli_set_charset($conn,"utf8");
   if ($conn->connect_error) {
     die("Sorry, connection failed" . $conn->connect_error);
+  }
+  
+  $spl = "SELECT * FROM customers";
+  $result = mysqli_query($conn, $sql);
+  if (mysqli_num_rows($result) > 0){
+      while ($row = mysqli_fetch_assoc($result)){
+          echo $row['customerNumber']."<br>";
+      }
   }
 ?>
 
